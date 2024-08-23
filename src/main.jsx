b9035ef1d0580810a17ba7f1b1cc5b66f5b06e92
hamzakebir <hamzux200@gmail.com>
2024-08-23 14:56:21 +0100

    Adding the language Changer an a navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import global_en from './languages/en/global.json'
+import global_fr from './languages/fr/global.json'
+import i18next from "i18next"
+import { I18nextProvider } from 'react-i18next';
+
+i18next.init({
+  interpolation : {escapeValue : false},
+  lng : "en",
+  resources : {
+    en : {
+      global : global_en
+    },
+    fr : {
+      global : global_fr
+    }
+  }
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <I18nextProvider i18n = {i18next}>
+      <App />
+    </I18nextProvider>
   </StrictMode>,
 )
