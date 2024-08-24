da14313dd20033b2e1ccc468cb5e5c7719f86b8c
hamzakebir <hamzux200@gmail.com>
2024-08-24 15:04:55 +0100

    Change the Hero Background and the navbar style

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import hamza from '../images/kebir.jpeg';
 import Typed from 'typed.js';
+import video from '../images/video.mp4';
 
 const Hero = () => {
     useEffect(() => {
@@ -19,8 +20,21 @@ const Hero = () => {
     }, []);
 
     return (
-        <div id="home" className="w-full h-full bg-gray-900 dark:bg-gray-200">
-            <div className="h-screen w-full mx-auto py-20 xl:px-16 xs:px-8 flex flex-col md:flex-row gap-4 justify-center items-center pb-10 pt-4">
+        <div id="home" className="relative w-full h-screen bg-gray-900 dark:bg-gray-200">
+            {/* Background Video */}
+            <video
+                src={video}
+                autoPlay
+                loop
+                muted
+                className="absolute top-0 left-0 w-full h-full object-cover z-0"
+            />
+
+            {/* Overlay */}
+            <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50 z-10"></div>
+
+            {/* Content */}
+            <div className="relative z-20 h-full flex flex-col md:flex-row gap-4 justify-center items-center px-8 md:px-16 py-20">
                 <div className="w-full md:w-1/2 flex justify-center md:justify-start">
                     <img
                         className="rounded-full md:max-w-[70%] sm:max-w-[40%] xs:max-w-[30%] md:block hidden"
@@ -30,8 +44,12 @@ const Hero = () => {
                 </div>
                 <div className="w-full md:w-1/2 flex flex-col justify-center items-center md:items-start text-white dark:text-gray-800">
                     <h1 className="text-4xl font-semibold font-serif text-center md:text-left">Hello, I'm KEBIR Hamza</h1>
-                    <h3 className="capitalize text-rose-400 text-center md:text-left">I'm <span className="typing text-green-500 dark:text-green-700">fullstack developer</span></h3>
-                    <p className="text-center md:text-left">Junior Full Stack Web Developer specializing in React.js and Laravel. Passionate about web development, I create responsive, efficient web applications and I'm eager to take on new challenges to grow my skills and contribute to impactful projects.</p>
+                    <h3 className="capitalize text-rose-400 text-center md:text-left">
+                        I'm <span className="typing text-green-500 dark:text-green-700">fullstack developer</span>
+                    </h3>
+                    <p className="text-center md:text-left">
+                        Junior Full Stack Web Developer specializing in React.js and Laravel. Passionate about web development, I create responsive, efficient web applications and I'm eager to take on new challenges to grow my skills and contribute to impactful projects.
+                    </p>
                     <div className="sm:mt-4 xs:mt-2">
                         <button className="px-6 py-1 bg-rose-500 text-white rounded-sm">Get In Touch</button>
                     </div>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import logo from '../images/logo.png';
 
 const Navbar = () => {
     const [t, i18n] = useTranslation('global');
     const [activeSection, setActiveSection] = useState('');
+    const [isScrolled, setIsScrolled] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            // Check if the page is scrolled more than 50px
+            if (window.scrollY > 50) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        };
+
+        // Add scroll event listener
+        window.addEventListener('scroll', handleScroll);
+
+        // Clean up the event listener on component unmount
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
 
     const handleChangeLanguage = (lang) => {
         i18n.changeLanguage(lang);
@@ -14,23 +34,28 @@ const Navbar = () => {
         setActiveSection(section);
     };
 
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
     };
 
     return (
-        <div className="fixed top-0 left-0 w-full bg-gray-900 text-white shadow-lg z-50">
-            {/* {t("message")}
-            <button onClick={() => handleChangeLanguage("en") } >En</button>
-            <button onClick={() => handleChangeLanguage("fr") } >Fr</button> */}
+        <div
+            className={`fixed top-0 left-0 w-full z-50 transition-colors duration-300 ${
+                isScrolled ? 'bg-gray-900 text-white shadow-lg' : 'bg-transparent text-white'
+            }`}
+        >
             <header className="lg:px-16 px-4 flex flex-wrap items-center py-4">
                 <div className="flex-1 flex justify-between items-center">
                     <span className="text-white font-bold text-3xl">KEBIR</span>
                 </div>
                 <label htmlFor="menu-toggle" className="pointer-cursor md:hidden block">
-                    <svg className="fill-current text-gray-200" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+                    <svg
+                        className="fill-current text-gray-200"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="20"
+                        height="20"
+                        viewBox="0 0 20 20"
+                    >
                         <title>menu</title>
                         <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
                     </svg>
@@ -41,7 +66,9 @@ const Navbar = () => {
                         <ul className="md:flex items-center justify-between text-base text-gray-100 dark:text-gray-600 pt-4 md:pt-0">
                             <li>
                                 <a
-                                    className={`md:p-4 py-3 px-0 block ${activeSection === 'home' ? 'text-rose-500' : ''}`}
+                                    className={`md:p-4 py-3 px-0 block ${
+                                        activeSection === 'home' ? 'text-rose-500' : ''
+                                    }`}
                                     href="#home"
                                     onClick={() => handleClick('home')}
                                 >
@@ -50,7 +77,9 @@ const Navbar = () => {
                             </li>
                             <li>
                                 <a
-                                    className={`md:p-4 py-3 px-0 block ${activeSection === 'about' ? 'text-rose-500' : ''}`}
+                                    className={`md:p-4 py-3 px-0 block ${
+                                        activeSection === 'about' ? 'text-rose-500' : ''
+                                    }`}
                                     href="#about"
                                     onClick={() => handleClick('about')}
                                 >
@@ -59,7 +88,9 @@ const Navbar = () => {
                             </li>
                             <li>
                                 <a
-                                    className={`md:p-4 py-3 px-0 block ${activeSection === 'education' ? 'text-rose-500' : ''}`}
+                                    className={`md:p-4 py-3 px-0 block ${
+                                        activeSection === 'education' ? 'text-rose-500' : ''
+                                    }`}
                                     href="#education"
                                     onClick={() => handleClick('education')}
                                 >
@@ -68,7 +99,9 @@ const Navbar = () => {
                             </li>
                             <li>
                                 <a
-                                    className={`md:p-4 py-3 px-0 block ${activeSection === 'experience' ? 'text-rose-500' : ''}`}
+                                    className={`md:p-4 py-3 px-0 block ${
+                                        activeSection === 'experience' ? 'text-rose-500' : ''
+                                    }`}
                                     href="#experience"
                                     onClick={() => handleClick('experience')}
                                 >
@@ -77,7 +110,9 @@ const Navbar = () => {
                             </li>
                             <li>
                                 <a
-                                    className={`md:p-4 py-3 px-0 block ${activeSection === 'projects' ? 'text-rose-500' : ''}`}
+                                    className={`md:p-4 py-3 px-0 block ${
+                                        activeSection === 'projects' ? 'text-rose-500' : ''
+                                    }`}
                                     href="#projects"
                                     onClick={() => handleClick('projects')}
                                 >
@@ -86,7 +121,9 @@ const Navbar = () => {
                             </li>
                             <li>
                                 <a
-                                    className={`md:p-4 py-3 px-0 block ${activeSection === 'contact' ? 'text-rose-500' : ''}`}
+                                    className={`md:p-4 py-3 px-0 block ${
+                                        activeSection === 'contact' ? 'text-rose-500' : ''
+                                    }`}
                                     href="#contact"
                                     onClick={() => handleClick('contact')}
                                 >
